87711d56f683d233808d6127fa53b5a31e4b4784
7dngur7 <7dngur7@naver.com>
2021-01-10 05:17:25 +0900

    sign in

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newaccount, setNewaccount] = useState(true);
+    const [error, setError] = useState("");
+
     const onChange = (event) => {
         const {
           target: { name, value },
@@ -13,9 +17,25 @@ const Auth = () => {
           setPassword(value);
         }
       };
-      const onSubmit = (event) => {
+      const onSubmit = async (event) => {
         event.preventDefault();
-      };
+        try {
+            let data;
+            if (newaccount) {
+              data = await authService.createUserWithEmailAndPassword(
+                email,
+                password
+              );
+            } else {
+              data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
+        const toggleAccount = () => setNewaccount((prev) => !prev);
+
 
 
     return(
@@ -27,8 +47,15 @@ const Auth = () => {
         <input name = "password" type = "password" placeholder = "password" required
          value={password} onChange={onChange}
         />
-        <input type = "submit" value = "Log in" />
+        <input
+          type="submit"
+          value={newaccount ? "Create Account" : "Sign In"}
+          />
+            {error}
     </form>
+    <span onClick={toggleAccount}>
+        {newaccount ? "Sign In" : "Create Account"}
+      </span>
     <button>Continue with Google</button>
     <button>Continue with Github</button>
 </div>
